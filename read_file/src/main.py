d28a57fd0065a6f4a62da9297949a066647ab7ee
Quang Thinh <nguyenquangthinh02012004@gmail.com>
2025-05-11 16:09:57 +0700

    Add NER

--- read_file/src/main.py
+++ read_file/src/main.py
@@ -66,46 +66,46 @@ def file_to_json(file_path, output_dir):
     endTime = time.time()
     print("Processing time:", endTime - startTime)
     
-if __name__ == "__main__":
+# if __name__ == "__main__":
     
-    startTime = time.time()
+#     startTime = time.time()
     
-    file_path = r"C:\MaHoa\read_file\test_files\test_pdf.pdf"
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    output_dir = os.path.join(script_dir, "output")
+#     file_path = r"C:\MaHoa\read_file\test_files\test_pdf.pdf"
+#     script_dir = os.path.dirname(os.path.abspath(__file__))
+#     output_dir = os.path.join(script_dir, "output")
     
-    texts, ocr = process_file(file_path)
-    pages = len(texts)
-    if texts:
-        for page in range(pages):
-            text = texts[page]
-            # print("Extracted text:", text)
-            entities = process_text(text)
-            output_path = entities_to_json(entities, output_dir=output_dir, filename=f"page_{page + 1}.json")
-            # print(entities)
-            # print(f"Entities saved to: {output_path}")
-    else:
-        print("Failed to extract text from the file Or no text found.")
+#     texts, ocr = process_file(file_path)
+#     pages = len(texts)
+#     if texts:
+#         for page in range(pages):
+#             text = texts[page]
+#             # print("Extracted text:", text)
+#             entities = process_text(text)
+#             output_path = entities_to_json(entities, output_dir=output_dir, filename=f"page_{page + 1}.json")
+#             # print(entities)
+#             # print(f"Entities saved to: {output_path}")
+#     else:
+#         print("Failed to extract text from the file Or no text found.")
         
-    if ocr:
-        private_img = []
-        for result in ocr:
-            page = result["page"]
-            image_index = result["image_index"]
-            text = result["text"]
-            has_face = result["has_face"]
+#     if ocr:
+#         private_img = []
+#         for result in ocr:
+#             page = result["page"]
+#             image_index = result["image_index"]
+#             text = result["text"]
+#             has_face = result["has_face"]
             
-            if has_private_info(result, process_text):
-                private_img.append({"page": page, "image_index": image_index})
+#             if has_private_info(result, process_text):
+#                 private_img.append({"page": page, "image_index": image_index})
                 
-        if private_img:
-            output_path = os.path.join(output_dir, "private_info_images.json")
-            with open(output_path, "w") as f:
-                json.dump(private_img, f, indent=4)
-            # print(f"Private images info saved to: {output_path}")
+#         if private_img:
+#             output_path = os.path.join(output_dir, "private_info_images.json")
+#             with open(output_path, "w") as f:
+#                 json.dump(private_img, f, indent=4)
+#             # print(f"Private images info saved to: {output_path}")
             
-    else:
-        print("No text and face found.")
+#     else:
+#         print("No text and face found.")
         
-    endTime = time.time()
-    print("Tortal processing time:", endTime - startTime)
+#     endTime = time.time()
+#     print("Tortal processing time:", endTime - startTime)
